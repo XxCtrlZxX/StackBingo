51b6dc9e21467ae8d442c372c530d06bd97db184
Jung haesung <48352078+XxCtrlZxX@users.noreply.github.com>
2020-05-25 14:54:09 +0900

    2020-05-25
    bug fix
    release exe file upload

--- StackBingo.cpp
+++ StackBingo.cpp
@@ -13,20 +13,37 @@ void gotoxy(short x, short y) { // 좌표 이동
 	COORD pos = { x, y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
+
 void printBoard();
 void clearBoard();
-int win();	// 몇픽이 승리 했는지 알려줌
+int win();		// 몇픽이 승리 했는지 알려줌
+bool win_Judge(int x, int y);
+
+class XY {
+public:
+	int x;
+	int y;
+	bool flag;
+	void set(int x_, int y_, bool flag_) {
+		x = x_;
+		y = y_;
+		flag = flag_;
+	}
+};
+
+XY H, V, LD, RD;
 
 int main()
 {
 	int input = 0, count = 1, winner = 0;
 	std::string name[3];
-
+	
+	system("mode con cols=50 lines=15");
+	
 	cout << "닉네임을 적어주세요" << endl;
 	cout << "1픽 : "; cin >> name[1];
 	cout << "2픽 : "; cin >> name[2];
 	
-	system("mode con cols=50 lines=15");
 	while (true) {
 		system("cls");
 		printBoard();
@@ -43,7 +60,7 @@ int main()
 				continue;
 			}
 			else if (ch == 'n' || ch == 'N') {
-				cout << "게임을 종료합니다" << endl;
+				cout << "\n게임을 종료합니다" << endl;
 				break;
 			}
 			else
@@ -74,32 +91,78 @@ int main()
 }
 
 int win() {
-	bool horizontal = false;
-	bool vertical = true;
-	bool diagnalLeftDown = true;	// diagnal : 대각선
-	bool diagnalRightDown = true;
-
-	for (int y = 0; y < HEIGHT; y++) {
-		for (int x = 0; x < WIDTH; x++) {
-			if (board[y][x] != 0) 
-			{
-				if (x + WINCOUNT - 1 <= WIDTH && board[y][x] == board[y][x + 1] && board[y][x] == board[y][x + 2] && board[y][x] == board[y][x + 3])
-					horizontal = true;
-				for (int i = 1; i < WINCOUNT; i++)
-				{
-					if (y + WINCOUNT - 1 >= HEIGHT || board[y][x] != board[y + i][x])
-						vertical = false;
-					if (x - WINCOUNT <= 0 || y + WINCOUNT - 1 >= HEIGHT || board[y][x] != board[y + i][x - i])
-						diagnalLeftDown = false;
-					if (x + WINCOUNT - 1 >= WIDTH || y + WINCOUNT - 1 >= HEIGHT || board[y][x] != board[y + i][x + i])
-						diagnalRightDown = false;
-				}
-				if (horizontal || vertical || diagnalLeftDown || diagnalRightDown)
+	for (int y = 0; y < HEIGHT; y++)
+		for (int x = 0; x < WIDTH; x++)
+			if (board[y][x] != 0)
+				if (win_Judge(x, y)) {
+					// TODO : 빙고 된 부분 빨간색으로 칠하는 함수
 					return board[y][x];
-			}
-		}
-	}
+				}
 	return 0;
+}
+
+//*****************//
+bool win_Judge(int x, int y) {
+
+	H.set(x, y, true);
+	V.set(x, y, true);
+	LD.set(x, y, true);
+	RD.set(x, y, true);
+
+	int cnt = 0;
+	while (H.flag || V.flag || LD.flag || RD.flag) {
+
+		cnt++;
+
+		// horizontal
+		if (H.x + 1 < WIDTH) {
+			if (board[H.y][H.x + 1] == board[H.y][H.x])
+				H.x++;
+			else
+				H.flag = false;
+		}
+		else
+			H.flag = false;
+
+		// vertical
+		if (V.y + 1 < HEIGHT) {
+			if (board[V.y + 1][V.x] == board[V.y][V.x])
+				V.y++;
+			else
+				V.flag = false;
+		}
+		else
+			V.flag = false;
+
+		// diagnal Left Down
+		if (LD.x > 0 && LD.y + 1 < HEIGHT) {
+			if (board[LD.y + 1][LD.x - 1] == board[LD.y][LD.x]) {
+				LD.x--;
+				LD.y++;
+			}
+			else
+				LD.flag = false;
+		}
+		else
+			LD.flag = false;
+
+		// diagnal Right Down
+		if (RD.x > 0 && RD.y + 1 < HEIGHT) {
+			if (board[RD.y + 1][RD.x + 1] == board[RD.y][RD.x]) {
+				RD.x++;
+				RD.y++;
+			}
+			else
+				RD.flag = false;
+		}
+		else
+			RD.flag = false;
+	}
+
+	if (cnt == WINCOUNT)
+		return true;
+
+	return false;
 }
 
 void printBoard() {
